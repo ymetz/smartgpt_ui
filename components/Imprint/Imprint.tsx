aa926268c832496cf15a0aced43bd6eb5f2177f8
Yannick Metz <yannick.metz@uni-konstanz.de>
2024-04-10 23:34:32 +0200

    fixed typo in privacy policy

--- components/Imprint/Imprint.tsx
+++ components/Imprint/Imprint.tsx
@@ -113,10 +113,6 @@ const PrivacyPolicy = () => {
         <h3>Types of Data Collected</h3>
         <h4>Personal Data</h4>
         <p>While using Our Service, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify You. Personally identifiable information may include, but is not limited to:</p>
-        <ul>
-          <li>Usage Data</li>
-        </ul>
-        <h4>Usage Data</h4>
         <p>Usage Data is collected automatically when using the Service.</p>
         <p>Usage Data may include information such as Your Device&apos;s Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</p>
         <p>When You access the Service by or through a mobile device, We may collect certain information automatically, including, but not limited to, the type of mobile device You use, Your mobile device unique ID, the IP address of Your mobile device, Your mobile operating system, the type of mobile Internet browser You use, unique device identifiers and other diagnostic data.</p>
